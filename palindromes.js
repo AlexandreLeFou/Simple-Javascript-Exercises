d41fad6607767623db886cb35c3b826144629859
AlexandreLeFou <alehunter@hotmail.gr>
2018-10-15 14:30:56 +0300

    need to fix if condition, see letters array backwards

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
-
+const palindromes = function(x) {
+    //var i,flag;
+   var letters = x.split("(?!^)")
+    //letters = x.split("")
+    var word=""
+    for (i=letters.length-1; i>=0; i--){ word += letters[i];}
+    //return word[word.length-1];
+    var k=word.length-1;
+    for (i=0; i<=word.length; i++){
+        
+        if (word[i]==letters[k]){
+            flag = true;}
+        else {flag= false;}
+    }
+    return flag;
 }
 
 module.exports = palindromes
+//racecar
