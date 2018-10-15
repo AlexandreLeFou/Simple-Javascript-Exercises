07052b683e794624212973a70ce940ffc8ffd7b0
AlexandreLeFou <alehunter@hotmail.gr>
2018-10-15 18:40:20 +0300

    fixed word parser need to make each elemetn to compare with other

--- palindromes.js
+++ palindromes.js
@@ -1,18 +1,20 @@
 const palindromes = function(x) {
-    //var i,flag;
-   var letters = x.split("(?!^)")
-    //letters = x.split("")
-    var word=""
-    for (i=letters.length-1; i>=0; i--){ word += letters[i];}
-    //return word[word.length-1];
-    var k=word.length-1;
-    for (i=0; i<=word.length; i++){
-        
-        if (word[i]==letters[k]){
-            flag = true;}
-        else {flag= false;}
-    }
-    return flag;
+
+//https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
+var i,flag;
+x='ZZZZ car, a man, a maraca.';
+var wordgiven = x.split("");
+//var wordreversed = wordgiven.reverse();
+//var joinedwordreverse= wordreversed.join("");
+for (i=0;i<wordgiven.length-1;i++){
+	//console.log(wordgiven[(wordgiven.length)-1-i]);    
+if (wordgiven[i] === wordgiven[(wordgiven.length)-1-i]){flag= true;}
+    else{flag= false;}
+}
+
+return flag;
+//return joinedwordreverse;
+
 }
 
 module.exports = palindromes
