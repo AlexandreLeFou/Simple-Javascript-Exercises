d69cff87270ee113ea3b96c23dc4abc54722ea06
AlexandreLeFou <alehunter@hotmail.gr>
2018-10-15 18:46:03 +0300

    used regular exression and lowercase to solve it,done

--- palindromes.js
+++ palindromes.js
@@ -1,19 +1,15 @@
 const palindromes = function(x) {
 
 //https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
-var i,flag;
-x='ZZZZ car, a man, a maraca.';
-var wordgiven = x.split("");
-//var wordreversed = wordgiven.reverse();
-//var joinedwordreverse= wordreversed.join("");
-for (i=0;i<wordgiven.length-1;i++){
-	//console.log(wordgiven[(wordgiven.length)-1-i]);    
-if (wordgiven[i] === wordgiven[(wordgiven.length)-1-i]){flag= true;}
-    else{flag= false;}
-}
-
-return flag;
-//return joinedwordreverse;
+  var noneedthese = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
+  
+  var stringprocess = x.toLowerCase().replace(noneedthese, '');
+ 
+  var reversestring = stringprocess.split('').reverse().join(''); 
+  
+  
+  
+  return reversestring === stringprocess; 
 
 }
 
