41140e5fba1d757a12e212238e66adacd85bda98
AlexandreLeFou <alehunter@hotmail.gr>
2019-01-06 01:08:56 +0200

    caesar final solution is ok

--- consolewebtest.js
+++ consolewebtest.js
@@ -1,11 +1,11 @@
-word='aaaAAA'
-inputshift= 555
-var i,check,j=0;
+word='aaa,jAAA'
+inputshift= 655
+var i,j=0;
 var newword=[];
 word=word.split("");
 var characterAscii= new RegExp("[a-zA-Z]");
 
-wordindex = word.length -1 ;// we start from 0 
+var wordindex = word.length -1 ;// we start from 0 
 
 
 //a-z->97-122
@@ -15,23 +15,26 @@ for (i=0;i<=wordindex;i++){
     wordloop =word[i].charCodeAt();
 
     finalWordIndex =wordloop+inputshift //abs for negative values
- if (finalWordIndex<0){finalWordIndex=Math.abs(finalWordIndex)} // if shift number is a negative
-    console.log("---> word loop:"+i)
-    
- if (characterAscii.test(word[i])){
-    if (word[i].toUpperCase() == word[i]){    
-      while (finalWordIndex>90){ finalWordIndex=finalWordIndex%90} //if shift number is bigger than a->z numbers       
-       finalWordIndex += 65;
-        newword[i] = String.fromCharCode(finalWordIndex);
-    
-      }else{
-      //else if ((word[i].toLowerCase() == word[i])){
-            while (finalWordIndex>122){ finalWordIndex=finalWordIndex%122} //if shift number is bigger than A->z numbers       
-            finalWordIndex += 97;
-            newword[i] = String.fromCharCode(finalWordIndex);
-          }
-   }else {newword[i] = word[i]} //if it's a not a char leave it as it is
+  console.log("---> word loop:"+i)
 
+ if (characterAscii.test(word[i])){
+ 
+  if (finalWordIndex<0){finalWordIndex=Math.abs(finalWordIndex)} // if shift number is a negative
+  
+    if (word[i].toUpperCase() == word[i]){
+      if (finalWordIndex>=65 && finalWordIndex<=90){
+        finalWordIndex = (((finalWordIndex - 65 + inputshift) % 26) + 65);
+      }
+
+    }else{
+     if (finalWordIndex>=97 && finalWordIndex<=122){
+      finalWordIndex = (((finalWordIndex - 97 + inputshift) % 26) + 97);
+    }
+    }
+    newword[i] = String.fromCharCode(finalWordIndex);
+  }else{
+  newword[i] = word[i]; //if it's a not a char leave it as it is
+  }
    console.log("Loop:"+i+", number:"+finalWordIndex)
     
 }    
